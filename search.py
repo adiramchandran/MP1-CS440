633658f59e58f0dc55f82d4829f36cc20ea3b8b8
Adi <adarsh.ramchandran@gmail.com>
2019-01-29 18:18:15 -0600

    bfs first try

--- search.py
+++ search.py
@@ -14,7 +14,8 @@ within this file -- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
 
-
+from maze import *
+import queue
 # Search should return the path and the number of states explored.
 # The path should be a list of tuples in the form (row, col) that correspond
 # to the positions of the path taken by your search algorithm.
@@ -30,12 +31,72 @@ def search(maze, searchMethod):
         "astar": astar,
     }.get(searchMethod)(maze)
 
+"""
+    # Function to print a BFS of graph
+    def BFS(self, s):
 
+        # Mark all the vertices as not visited
+        visited = [False] * (len(self.graph))
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print (s, end = " ")
+
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+https://crab.rutgers.edu/~guyk/BFS.pdf
+"""
 def bfs(maze):
     # TODO: Write your code here
     # return path, num_states_explored
-    return [], 0
+    # getNeighbors(maze, row, col) --> give us coordinates to place into the queue/path
+    # getStart(maze) --> gives us first coordinates
+    start = getStart(maze)
+    q = queue.Queue()
+    num_states_explored = 0
+    visited = []
+    prev = []
+    q.put(start)
+    prev[start] = None          # no previous for starting coordinate
+    visited.append(start)
+    dot_coord = None            # coordinate of dot (objective)
+    while !(q.empty()):
+        v = q.get()
+        if isObjective(maze, v[0], v[1]):   # check if dot is found
+            dot_coord = v                   # update coordinate of dot
+            break
+        for i in getNeighbors(maze, v[0], v[1]):
+            if i not in visited:
+                q.put(i)
+                prev[i] = v    # set previous node for each visited node
+                visited.append(i)
 
+    path = []
+    path.append(dot_coord)
+    p = prev[dot_coord]
+    while p != None:
+        path.append(p)
+        p = prev[p]
+    path = path[::-1]
+    num_states_explored = len(visited)
+    return path, num_states_explored
 
 def dfs(maze):
     # TODO: Write your code here
